7e03b5d5a27a6b17c3c5667e8d8c33787b5b6d01
Adeon18 <56467999+Adeon18@users.noreply.github.com>
2021-04-26 18:20:31 +0300

    Finished Stack

---
+++ Stack/stack_to_queue.py
@@ -0,0 +1,36 @@
+"""
+Stack to queue converter.
+"""
+import copy
+
+from arraystack import ArrayStack   # or from linkedstack import LinkedStack
+from arrayqueue import ArrayQueue   # or from linkedqueue import LinkedQueue
+
+
+def stack_to_queue(stack):
+    """
+    Convert queue to a stack
+    """
+    input_stack = copy.deepcopy(stack)
+    output_queue = ArrayQueue()
+
+    while True:
+        try:
+            output_queue.add(input_stack.pop())
+        except KeyError:
+            break
+    
+    return output_queue
+
+if __name__ == '__main__':
+    stack = ArrayStack()
+    for i in range(10):
+        stack.add(i)
+    queue = stack_to_queue(stack)
+    print(queue.pop())
+    print(stack.pop())
+
+    stack.add(11)
+    queue.add(11)
+    print(queue)
+    print(stack)
